d92b8f72261224484e78b22d50d9796bf5e62d50
Wahidullah Rahimi <wahidrahimi45@gmail>
2024-05-17 12:21:53 -0700

    @Before @After

--- src/main/java/com/wahid/learnspringaop/aopexample/aspects/LogginAspect.java
+++ src/main/java/com/wahid/learnspringaop/aopexample/aspects/LogginAspect.java
@@ -1,6 +1,9 @@
 package com.wahid.learnspringaop.aopexample.aspects;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,12 +21,22 @@ public class LogginAspect {
 
     // execution(* PACKAGE.*.*(..)) //intercept all the method calls that are happening in classes withing this package
     // execution(* com.wahid.learnspringaop.aopexample.business.*.*(..)) // intercept execution of all bean in this package
-    @Pointcut("")
-    public void logMethodCall(){
+    @Before("execution(* com.wahid.learnspringaop.aopexample.*.*.*(..))")
+    public void logMethodCallBeforeExecution(JoinPoint joinPoint){
         // we are going to intercept all the method calls that are going to the business and data layer beans
 
         //Logic: what to do?
-        logger.info("Method is called - ");
+        logger.info("Before Aspect: Method {} method is called with arguments - {}", joinPoint, joinPoint.getArgs());
+
+    }
+
+    @After("execution(* com.wahid.learnspringaop.aopexample.*.*.*(..))")
+    public void logMethodCallAfterExecution(JoinPoint joinPoint){
+        // we are going to intercept all the method calls that are going to the business and data layer beans
+
+        //Logic: what to do?
+        logger.info("After Aspect: Method {} method is called with arguments - {}", joinPoint, joinPoint.getArgs());
+
 
     }
 }
